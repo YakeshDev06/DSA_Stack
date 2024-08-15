1f21d129c4dca80d66abcc989825c9bcdbcf0558
YakeshDev06 <yagapriyan8@gmail.com>
2024-08-15 16:30:45 +0530

    My first native project commit

---
+++ src/main/java/com/ypdev/dsa/bundle/DsaBasicMathProblems.java
@@ -0,0 +1,20 @@
+package com.ypdev.dsa.bundle;
+
+public class DsaBasicMathProblems {
+
+	public void findOddEven(long no) {
+
+		if (no % 2 == 0) {
+			System.out.println("Given no " + no + " is even");
+		} else {
+			System.out.println("Given no " + no + " is odd");
+		}
+
+	}
+
+	public static void main(String[] args) {
+		DsaBasicMathProblems dsa = new DsaBasicMathProblems();
+		dsa.findOddEven(21);
+	}
+
+}

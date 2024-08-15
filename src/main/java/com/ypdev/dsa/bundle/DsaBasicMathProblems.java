4c117a71770956d9bedfa82166d2c30edfc9ea0d
Yagapriyan <150065859+YakeshDev06@users.noreply.github.com>
2024-08-15 16:33:42 +0530

    Update DsaBasicMathProblems.java

--- src/main/java/com/ypdev/dsa/bundle/DsaBasicMathProblems.java
+++ src/main/java/com/ypdev/dsa/bundle/DsaBasicMathProblems.java
@@ -12,7 +12,7 @@ public class DsaBasicMathProblems {
 
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { //Git pull config
 		DsaBasicMathProblems dsa = new DsaBasicMathProblems();
 		dsa.findOddEven(21);
 	}
